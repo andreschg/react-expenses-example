b64856d43eb8988d85f4a10d616ed805ab67162e
Andres Chaves <andres1009_93@hotmail.com>
2018-04-04 19:06:20 -0600

    First commit.

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Workin clean</p>, document.getElementById('app'));
