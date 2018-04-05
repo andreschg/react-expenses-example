98b5b91d91bcfa0fc7d2137e23b0bfa1cf98e67a
Andres Chaves <andres1009_93@hotmail.com>
2018-04-04 19:36:26 -0600

    Added some routes with dummy content.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Workin clean</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard page.
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from expense component.
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from edit expense component.
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from help component.
+  </div>
+);
+
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage} exact={true}/>
+      <Route path="/edit" component={EditExpensePage} exact={true}/>
+      <Route path="/help" component={HelpPage} exact={true}/>
+    </div>
+    
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
