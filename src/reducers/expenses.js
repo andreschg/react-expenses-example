c86d5c76234b9a6eea429007de5a05ae8efc6abf
Andres Chaves <andres1009_93@hotmail.com>
2018-04-08 13:30:38 -0600

    Reducers files added.

---
+++ src/reducers/expenses.js
@@ -0,0 +1,25 @@
+// Expenses reducer
+const expensesReducerDefaultState = [];
+
+export default (state = expensesReducerDefaultState , action) => {
+  console.log(`Running on expenses Reducer: ${action.type}`);
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ]
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return { ...expense, ...action.updates }
+        } else {
+          return expense;
+        }
+      });
+    default:
+      return state;
+  }
+};
---
+++ src/reducers/filters.js
@@ -0,0 +1,34 @@
+// Filter reducer
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+}
+
+export default (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text 
+      };
+    case 'SET_SORT_BY':
+      return {
+        ...state,
+        sortBy: action.sortBy
+      }
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.date
+      }
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.date
+      }
+    default:
+      return state;
+  }
+};
