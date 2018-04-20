06afca3c180cf713e97b8ed92e84ea0fe57f0641
Andres Chaves <andres1009_93@hotmail.com>
2018-04-19 18:35:19 -0600

    Dummy data removed.

--- src/app.js
+++ src/app.js
@@ -12,15 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-// addExpense -> water bill
-store.dispatch(addExpense({ description: 'Water bill', amount: 500}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 400, createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 155}));
-
-const storeState = store.getState();
-
-console.log(store.getState());
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
